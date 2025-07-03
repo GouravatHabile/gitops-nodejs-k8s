55dbc13a1ccdf08d76cd760d5e530edd06206b03
GouravatHabile <sharma.gourav@habilelabs.io>
2025-07-03 12:47:43 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end("Hello from Node.js on Kubernetes via Argo CD!");
+  res.end("Hello from Node.js on Kubernetes via Argo CD for habilelabs");
 });
 
 server.listen(port, () => {
